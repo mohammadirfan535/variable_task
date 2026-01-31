2f1869940b9c6289cccfe1b935ad206d5b074962
mohammadirfan535 <mohammadksirfan@gmail.com>
2026-01-31 13:19:06 +0530

    variable_practice

---
+++ variable.py
@@ -0,0 +1,30 @@
+student_name="irfan"
+age=22
+is_student=True
+print(student_name)
+print(age)
+print(is_student)
+
+a,b,c=10,20,30
+print(a,b,c)
+
+'totalmarks' # valid: start with a letters
+
+'1st student' # invalid: variable name cannot start with a number
+
+'student-name' # invalid:'-' is not allowed
+
+'_count' # valid:start with an underscore
+
+'class_name' # invalid :class is reversed keword in python 
+
+'studentName' # valid:start with a letters allowed
+
+csk=50
+score=80
+print(csk)
+print(score)
+
+
+
+
